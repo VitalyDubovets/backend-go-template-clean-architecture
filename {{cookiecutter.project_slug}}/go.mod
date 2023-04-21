c75dde6949b8b7f17c225ad4bded2de75780acb4
vitaly_dubovets <dubovetsvitaliy@gmail.com>
2023-04-22 00:30:21 +0300

    Initialized base components

---
+++ {{cookiecutter.project_slug}}/go.mod
@@ -0,0 +1,7 @@
+module {{ cookiecutter.project_slug }}
+
+go 1.20
+
+require (
+	go.uber.org/zap v1.24.0
+)
---
+++ {{cookiecutter.project_slug}}/internal/core/error/error.go
@@ -0,0 +1,23 @@
+package error
+
+import (
+	"errors"
+	"strconv"
+)
+
+type BusinessError struct {
+	code   int
+	Detail string
+}
+
+func (e *BusinessError) Code() int {
+	return e.code
+}
+
+func (e *BusinessError) Error() string {
+	return e.Detail + ": error code " + strconv.Itoa(e.code)
+}
+
+func (e *BusinessError) WrapWithError(err error) error {
+	return errors.Join(e, err)
+}
