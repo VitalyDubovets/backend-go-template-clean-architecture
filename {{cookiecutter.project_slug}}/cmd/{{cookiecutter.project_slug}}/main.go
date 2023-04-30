15be125278c86a530c3086196dcb97d46092d569
vitaly_dubovets <dubovetsvitaliy@gmail.com>
2023-05-01 00:43:16 +0300

    Added traces

--- {{cookiecutter.project_slug}}/cmd/{{cookiecutter.project_slug}}/main.go
+++ {{cookiecutter.project_slug}}/cmd/{{cookiecutter.project_slug}}/main.go
@@ -41,21 +41,41 @@ func main() {
 		log.Fatalf("tracing.Init: %s", err)
 	}
 
-	otel.SetTracerProvider(tp)
-
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
+
+	otel.SetTracerProvider(tp)
+
+	tr := tp.Tracer("component-main")
+
+	ctx, span := tr.Start(ctx, "main")
+	defer span.End()
 
 	applicationTimeout := time.Duration(appConfig.ShutDownApplicationTimeout) * time.Second
 	waitCh := appshutdown.ShutDownApplication(ctx, applicationTimeout, map[string]appshutdown.Handler{
 		"sentry": func(ctx context.Context) error {
+			trs := tp.Tracer("component-sentry-shutdown")
+
+			ctx, spanSentry := trs.Start(ctx, "sentryShutdown")
+			defer spanSentry.End()
+
 			sentry.Flush(time.Duration(appConfig.SentryConfig.Timeout) * time.Second)
 			return nil
 		},
 		"tracing": func(ctx context.Context) error {
+			trs := tp.Tracer("component-tracing-shutdown")
+
+			ctx, spanSentry := trs.Start(ctx, "tracingShutdown")
+			defer spanSentry.End()
+
 			return tp.Shutdown(ctx)
 		},
 		"logger": func(ctx context.Context) error {
+			trs := tp.Tracer("component-logger-shutdown")
+
+			ctx, spanSentry := trs.Start(ctx, "loggerShutdown")
+			defer spanSentry.End()
+
 			return flushFn()
 		},
 	})
